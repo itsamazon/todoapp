83bf7a9faff511405bb9bd309c62c03ab180943c
chidera <chidera.diandra@gmail.com>
2021-11-21 22:28:44 +0100

    updates

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -45,17 +45,17 @@ const LoginForm = (props) => {
             <div className="rounded-md shadow-sm -space-y-px">
               <div>
                 <label htmlFor="email-address" className="sr-only">
-                  Email address
+                  Username
                 </label>
                 <input
-                  id="email-address"
+                  id="Username"
                   name="username"
                   type="text"
                   autoComplete="username"
                   required
                   onChange={handleChange}
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  placeholder="Username"
                 />
               </div>
               <div>
