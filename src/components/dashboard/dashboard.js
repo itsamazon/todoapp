a73dc67495220e8ab1ded79c0cf144432edcc857
chidera <chidera.diandra@gmail.com>
2021-11-21 23:23:56 +0100

    dashboard updated

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -8,7 +8,6 @@ import { useNavigate } from 'react-router-dom';
 
 
 const Dashboard = (props) => {
-    const dispatch = useDispatch()
     const navigate = useNavigate() 
     const signOut = () =>{
         navigate('/')
